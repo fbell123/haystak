9a224d4c745173d478824e914445281323857e42
Anna Beddow <anna.beddow@gmail.com>
2016-11-23 20:06:36 +0000

    added callback

--- needle.js
+++ needle.js
@@ -1,34 +1,37 @@
 window.onload = function(){
 console.log(0);
-retrieveGoogleUrls();
+retrieve(retrieveGoogleUrls);
 console.log(2);
 };
 
+
 var array = [];
 
-
-var compareArrays = function(){
-  var matches = []
-    for(var i=0; i<values.length; i++) {
-    matches.push(array.find(values[i]));
-    }
-}
-
-function getStorageUrls(){
-  chrome.extension.getBackgroundPage().getUrls();
-}
-
-var retrieveGoogleUrls = function(){
+var retrieveGoogleUrls = function(callback){
   console.log(1);
   var links = contentDocument.getElementsByTagName("a");
   for(var i=0; i<links.length; i++) {
    array.push(links[i].href);
  }
  console.log(array);
- return array
-}
-// 
-//
-// chrome.runtime.sendMessage({mesage: "array"}, array, function(response) {
-//   console.log(response.urlMatches)
-// });
+ callback();{
+   chrome.runtime.sendMessage({mesage: "array"}, array, function(response) {
+     console.log(response.urlMatches);
+   });
+    return array;
+ }
+};
+
+retrieve = function(retrieveGoogleUrls) {
+};
+
+  // var compareArrays = function(){
+  //   var matches = []
+  //     for(var i=0; i<values.length; i++) {
+  //     matches.push(array.find(values[i]));
+  //     }
+  // }
+  //
+  // function getStorageUrls(){
+  //   chrome.extension.getBackgroundPage().getUrls();
+  // }
