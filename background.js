f1951e0111bce45cf17b1d4d6e65d7fe22eada6b
Laurent Bouhnik <misterbl@hotmail.com>
2016-11-23 09:59:36 +0000

    more awsomness

--- background.js
+++ background.js
@@ -1,48 +1,9 @@
-// var enabled = false;
-//
-// chrome.browserAction.onClicked.addListener(function(){
-//   enabled = !enabled;
-//
-//   chrome.browserAction.setBadgeText({
-//     text: enabled? "ON" : ""
-//   });
-// });
-//
-
-//
-// var array = [];
-// chrome.runtime.onMessage.addListener(function (question) {
-//   console.log(array);
-//   if (array.length < 2) {
-//     array.push(question);
-//   }
-//   else
-//   storeQuestion(array[0]);
-//   storeUrl(arra[1]);
-//
-//   // if (request == "saveQuestion") {
-//     console.log("hello3");
-//   //   // storeQuestion(question);
-//   // }
-//   // else if (request == "saveUrl") {
-//   //   var url = question;
-//   //   storeUrl(url);
-//   // }
-// });
-//
-//
-// var search;
-
 var storeQuestion = function (question) {
-  console.log("hello4");
-localStorage.setItem('newest question', JSON.stringify(question));
+  localStorage.setItem('newest question', JSON.stringify(question));
 };
 
 var storeUrl= function (url) {
   var retrievedQuestion = JSON.parse(localStorage.getItem('newest question'));
-localStorage.setItem(retrievedQuestion, JSON.stringify(url));
-retrievedQuestion = nil;
+  localStorage.setItem(retrievedQuestion,url);
+  retrievedQuestion = nil;
 };
-// var testObject = { 'one': 1, 'two': 2, 'three': 3 };
-// localStorage.setItem('testObject', JSON.stringify(testObject));
-// var retrievedObject = JSON.parse(localStorage.getItem('testObject'));
