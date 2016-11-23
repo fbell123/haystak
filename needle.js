6500d1d9b728d1245f1bc2a2cb9f67139ecd5443
Anna Beddow <anna.beddow@gmail.com>
2016-11-23 20:19:10 +0000

    removed contentDocument as google links are not in iframe

--- needle.js
+++ needle.js
@@ -9,23 +9,24 @@ var array = [];
 
 var retrieveGoogleUrls = function(callback){
   console.log(1);
-  var links = contentDocument.getElementsByTagName("a");
+  var links = document.getElementsByTagName("a");
   for(var i=0; i<links.length; i++) {
    array.push(links[i].href);
  }
  console.log(array);
  callback();
   return array;
-  };
 };
 
 function messageBackground(){
-  chrome.runtime.sendMessage({mesage: "array"}, array, function(response) {
+  chrome.runtime.sendMessage({mesage: "array"}, array, function(response){
     console.log(response.urlMatches);
+    }
+  );
 }
 
 function retrieve(){
-    retrieveGoogleUrls(messageBackground)
+  retrieveGoogleUrls(messageBackground);
 }
 
   // var compareArrays = function(){
