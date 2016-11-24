587fc3f95523bbc03deeed0b9eabb4df3393d817
Laurent Bouhnik <misterbl@hotmail.com>
2016-11-24 15:29:11 +0000

    icon inserted

--- needle.js
+++ needle.js
@@ -1,8 +1,8 @@
-window.onload = function(){
+$(document).ready(function(){
 console.log(0);
-retrieve();
+injectNeedleIcon();
 console.log(2);
-};
+});
 
 
 var array = [];
@@ -10,7 +10,7 @@ var array = [];
 var matchedUrl = [];
 
 function retrieveGoogleUrls (){
-  console.log(1);
+  console.log(5);
   var links = document.getElementsByTagName("a");
   for(var i=0; i<links.length; i++) {
    array.push(links[i].href);
@@ -35,12 +35,19 @@ function retrieve(){
 // }
 
 
-function injectNeedleIcon(url){
-  var img = document.createElement('img');
-  img.src = 'logo.png';
-  var div = document.createElement('div');
-  div.appendChild(img);
+function injectNeedleIcon(){
+  url = ["https://en.wikipedia.org/wiki/Pepper", "https://www.peppergroup.co.uk/about-us/contact-us", "http://www.peppercorp.com/"];
   for(i=0; i<url.length; i++) {
-    document.querySelector("a[href='" +url[i] + "']").appendChild(div);
-  }
+  var img = document.createElement('img');
+  img.src = chrome.extension.getURL('logoicon.png');
+  // img.src = "https://s22.postimg.org/o1vf0n6z5/icon.png";
+  console.log(img.src);
+  console.log(1);
+  var div = document.createElement('span');
+  div.appendChild(img);
+  console.log(div);
+  console.log(3);
+    document.querySelector("a[href='" + url[i] + "']").appendChild(div);
+    console.log(4);
+}
 }
