e19ad42e33b70b7b11aef5261b8fb4774bc52f48
Laurent Bouhnik <misterbl@hotmail.com>
2016-11-24 11:40:27 +0000

    messages working

--- background.js
+++ background.js
@@ -9,13 +9,13 @@ var temporary = {
 };
 
 function storeQuestion (question) {
-   temporary["data"]["question"] = question;
+  temporary["data"]["question"] = question;
 }
 
 chrome.runtime.onMessage.addListener(
   function(request) {
     if (request.type === "save_highlight") {
-  saveHighlight(request.data);
+      saveHighlight(request.data);
     }
     else if (request.type === "save_question") {
       saveQuestion(request.data);
@@ -26,26 +26,21 @@ chrome.runtime.onMessage.addListener(
   });
 
 
-function storeUrl (url) {
-  temporary["url"] = url;
-  // chrome.tabs.executeScript( {
-  //               code: "window.getSelection().toString();"
-  //           }, function(selection) {
-  //               console.log(selection);
-  //           });
-  localStorage.setItem(temporary["url"],JSON.stringify(temporary["data"]));
-}
+  function storeUrl (url) {
+    temporary["url"] = url;
+    localStorage.setItem(temporary["url"],JSON.stringify(temporary["data"]));
+  }
 
-function search(question) {
-  question.split(' ').join('+');
-  chrome.tabs.update({"url": "https://www.google.co.uk/search?q="+question, "selected": true});
-}
+  function search(question) {
+    question.split(' ').join('+');
+    chrome.tabs.update({"url": "https://www.google.co.uk/search?q="+question, "selected": true});
+  }
 
-function saveQuestion(question){
-storeQuestion(question);
-  search(question);
-}
+  function saveQuestion(question){
+    storeQuestion(question);
+    search(question);
+  }
 
-function saveHighlight(selection) {
-  temporary["data"]["highlight"] = selection;
-}
+  function saveHighlight(selection) {
+    temporary["data"]["highlight"] = selection;
+  }
--- popup.js
+++ popup.js
@@ -14,23 +14,19 @@ function getUrl(){
     var url = tab.url;
     document.getElementById('currentLink').innerHTML = url;
     chrome.runtime.sendMessage({ type: "save_url", data: url});
-    // chrome.extension.getBackgroundPage().storeUrl(url);
   });
 }
 
 function questionSearch(){
   var question = document.getElementById('question').value;
   chrome.runtime.sendMessage({ type: "save_question", data: question});
-  // chrome.extension.getBackgroundPage().saveQuestion(question);
 }
 
 function getHighlight(callback) {
-chrome.tabs.executeScript( {
-               code: "window.getSelection().toString();"
-           }, function(selection) {
-             chrome.runtime.sendMessage({ type: "save_highlight", data: selection});
-              // chrome.extension.getBackgroundPage().saveHighlight(selection);
-          //  });
-           callback();
-        });
-      }
+  chrome.tabs.executeScript( {
+    code: "window.getSelection().toString();"
+  }, function(selection) {
+    chrome.runtime.sendMessage({ type: "save_highlight", data: selection});
+    callback();
+  });
+}
