59c935bf0f8ea3a05f720601f518da22a1ed8443
Frankie Bell <fbell.pro@gmail.com>
2016-11-25 09:55:50 +0000

    more changes

--- haystack.js
+++ haystack.js
@@ -4,14 +4,18 @@ window.onload = function () {
 };
 
 var listHistory = function (list) {
-  for (var i = 0; i < localStorage.length; i++) {
-    if (localStorage.key(i) == "newest question") { continue;}
-    var keyTitle = localStorage.key(i);
-    var keyValue = localStorage.getItem(keyTitle);
-    var obj = JSON.parse(keyValue);
-    var questionValue = obj.question;
-    var highlightValue = obj.highlight;
-    var questionAnswerPair = "<li>" + "<a href='" + keyTitle + "' target='_blank'>" + keyTitle + "</a>" + "<p>Your question: " + questionValue + "<br>Your selected answer: " + highlightValue + "</p><br><br></li>";
-    list.innerHTML += questionAnswerPair;
-  }
+ for (var i = 0; i < localStorage.length; i++) {
+   if (localStorage.key(i) == "newest question") { continue;}
+   var keyTitle = localStorage.key(i);
+   var keyValue = localStorage.getItem(keyTitle);
+   var obj = JSON.parse(keyValue);
+   var questionValue = obj.question;
+   var highlightValue = obj.highlight;
+   var questionAnswerPair = "<li>" + "<a href='" + keyTitle + "' target='_blank'>" + keyTitle + "</a>" + "<p>Your question: " + questionValue + "<br>Your selected answer: " + highlightValue + "</p><br><br></li>";
+   list.innerHTML += questionAnswerPair;
+ }
 };
+
+// var deleteListing = function(listing) {
+//   localStorage.removeItem(listing);
+// }; NOT WORKING
--- needle.js
+++ needle.js
@@ -1,6 +1,6 @@
-$(document).ready(function(){
+window.onload = function() {
   retrieve();
-});
+};
 
 var array = [];
 
