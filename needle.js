ce25ef1a7c0cbc0b7b8e0bb649b355af8bb6eb73
Felix Harrison <fmlharrison@gmail.com>
2016-11-27 13:24:41 +0000

    refactored niddle.js script

--- needle.js
+++ needle.js
@@ -3,6 +3,12 @@ $(document).ready(function(){
 });
 
 var array = [];
+var img = createElement('img');
+var popup = createElement("a");
+var div = createElement('span');
+var spacePopUp = createElement('div');
+var valueInfo = createElement('p');
+var valueHighlight = createElement('p');
 
 function retrieveGoogleUrls (callback){
   var links = document.getElementsByTagName("a");
@@ -32,30 +38,52 @@ function useMatches (response, callback) {
   callback(url, value);
 }
 
+function createElement(element) {
+  return document.createElement(element);
+}
+
+function imgAttribute(img) {
+  img.src = chrome.extension.getURL('logoicon.png');
+}
+
+function createAllAttributes(element1, element2, element3) {
+  element1.setAttribute("href", "#");
+  element1.setAttribute("id", "trigger");
+  element2.setAttribute("id", "container");
+  element3.setAttribute("id", "pop-up");
+}
+
+function insertImg(parentDiv, img) {
+  parentDiv.appendChild(img);
+}
+
+function htmlInsert(element1, element2, value) {
+  element1.innerHTML = "<div class='header'><div class='home-menu pure-menu pure-menu-horizontal pure-menu-fixed'><div class='pure-menu-heading' href=''>Haysta:k</div></div></div><ul class='pure-menu-list'><br><div class='para-header'>Q:</div>" + "<div class='para-content'>" + JSON.parse(value[i][0]).question + "</div>";
+  element2.innerHTML = "<div class='para-header'>A:</div>" + "<div class='para-content'>" + JSON.parse(value[i][0]).highlight + "</div>";
+}
+
+function insertIntoPopup(parentDiv, element1, element2) {
+  parentDiv.appendChild(element1);
+  parentDiv.appendChild(element2);
+}
+
+function insertIcon(url, iconDiv) {
+  var parentDiv = document.querySelector("a[href='" + url[i] + "']").parentNode;
+  var sp2 = document.querySelector("a[href='" + url[i] + "']");
+  parentDiv.insertBefore(iconDiv, sp2);
+}
+
 function injectNeedleIcon(url, value){
   url = [].concat.apply([], url);
   for(i=0; i<url.length; i++) {
-    var img = document.createElement('img');
-    img.src = chrome.extension.getURL('logoicon.png');
-    var popup = document.createElement("a");
-    popup.setAttribute("href", "#");
-    popup.setAttribute("id", "trigger");
-    popup.appendChild(img);
-    var div = document.createElement('span');
-    div.setAttribute("id", "container");
-    var spacePopUp = document.createElement('div');
-    spacePopUp.setAttribute("id", "pop-up");
-    var valueInfo = document.createElement('p');
-    valueInfo.innerHTML = "<div class='header'><div class='home-menu pure-menu pure-menu-horizontal pure-menu-fixed'><div class='pure-menu-heading' href=''>Haysta:k</div></div></div><ul class='pure-menu-list'><br><div class='para-header'>Q:</div>" + "<div class='para-content'>" + JSON.parse(value[i][0]).question + "</div>";
-    var valueHighlight = document.createElement('p');
-    valueHighlight.innerHTML = "<div class='para-header'>A:</div>" + "<div class='para-content'>" + JSON.parse(value[i][0]).highlight + "</div>";
-    spacePopUp.appendChild(valueInfo);
-    spacePopUp.appendChild(valueHighlight);
-    div.appendChild(popup);
-    div.appendChild(spacePopUp);
-    var parentDiv = document.querySelector("a[href='" + url[i] + "']").parentNode;
-    var sp2 = document.querySelector("a[href='" + url[i] + "']");
-    parentDiv.insertBefore(div, sp2);  }
+    imgAttribute(img);
+    createAllAttributes(popup, div, spacePopUp);
+    insertImg(popup, img);
+    htmlInsert(valueInfo, valueHighlight, value);
+    insertIntoPopup(spacePopUp, valueInfo, valueHighlight);
+    insertIntoPopup(div, popup, spacePopUp);
+    insertIcon(url, div);
+  }
   hovering();
 }
 
