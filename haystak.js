80d95eac81c6055f3df31977de3734b4bf74792a
Frankie Bell <fbell.pro@gmail.com>
2016-11-30 10:04:03 +0000

    search function added and working

--- haystak.js
+++ haystak.js
@@ -1,11 +1,9 @@
 
 window.onload = function () {
-  var searchTags = document.getElementById('tagSearchSubmit');
-  searchTags.addEventListener('click', tagSearch);
   var list = document.getElementById('search-history');
   listHistory(list);
-
 };
+
  var keyTitle;
 var listHistory = function (list) {
  for (var i = 0; i < localStorage.length; i++) {
@@ -17,36 +15,41 @@ var listHistory = function (list) {
    var highlightValue = obj.highlight;
    var tagValue = obj.tags;
    var timeValue = ((obj.date).substring(0, 10));
-   var questionAnswerPair = "<li>" + "<div class='content-container'>" + "<a href='" + keyTitle + "' target='_blank'>" + keyTitle + "</a>" + "<br><div class='para-header'>Q:</div>" + "<div class='para-content'>" + questionValue + "</div>" + "<div class='para-header'>A:</div>" + "<div class='para-content'>" + highlightValue +  "</div>" + "<div class='para-header'>Tags:</div>" + "<div class='para-content-tags'>" + tagValue + "</div><div class='para-header'>Created on:</div><div class='para-content-time'>" + timeValue;
+   var questionAnswerPair = "<li>" + "<div class='content-container'>" + "<a href='" + keyTitle + "' target='_blank'>" + keyTitle + "</a>" + "<br><div class='para-header'>Q:</div>" + "<div class='para-content'>" + questionValue + "</div>" + "<div class='para-header'>A:</div>" + "<div class='para-content'>" + highlightValue +  "</div>" + "<div class='para-header'>Tags:</div>" + "<div class='para-content-tags' id='tags'>" + tagValue + "</div>" + "</li><br>";
    list.innerHTML += questionAnswerPair;
 }
 };
 
+(function ($) {
+	  jQuery.expr[':'].Contains = function(a,i,m){
+		  return (a.textContent || a.innerText || "").toUpperCase().indexOf(m[3].toUpperCase())>=0;
+	  };
 
+	  function listFilter(header, list) {
+		var form = $("<form>").attr({"class":"filterform","action":"#"}),
+			input = $("<input>").attr({"class":"filterinput","type":"text","placeholder":"Search tags"});
+		$(form).append(input).appendTo(header);
 
-function tagSearch(){
-  var search = document.getElementById('tagSearch').value;
-  var tagHistory = document.getElementById("search-tags");
-  console.log(0);
-  for (var i = 0; i < localStorage.length; i++) {
-    keyTitle = localStorage.key(i);
-    var keyValue = localStorage.getItem(keyTitle);
-    var obj = JSON.parse(keyValue);
-    var questionValue = obj.question;
-    var highlightValue = obj.highlight;
-    var tagValue = obj.tags;
-    var timeValue = ((obj.date).substring(0, 10));
-    console.log(search);
-    console.log(1);
-    console.log(tagValue);
-    if (tagValue === search){
-    var questionAnswerPair = "<li>" + "<div class='content-container'>" + "<a href='" + keyTitle + "' target='_blank'>" + keyTitle + "</a>" + "<br><div class='para-header'>Q:</div>" + "<div class='para-content'>" + questionValue + "</div>" + "<div class='para-header'>A:</div>" + "<div class='para-content'>" + highlightValue +  "</div>" + "<div class='para-header'>Tags:</div>" + "<div class='para-content-tags'>" + tagValue + "</div><div class='para-header'>Created on:</div><div class='para-content-time'>" + timeValue;
-    console.log(questionAnswerPair);
-    tagHistory.innerHTML += questionAnswerPair;}
-}
-}
+		$(input)
+		  .change( function () {
+			var filter = $(this).val();
+			if(filter) {
+			  $(list).find("div#tags:not(:Contains(" + filter + "))").parent().slideUp();
+			  $(list).find("div#tags:Contains(" + filter + ")").parent().slideDown();
+			} else {
+			  $(list).find("li").slideDown();
+			}
+			return false;
+		  })
+		.keyup( function () {
+			$(this).change();
+		});
+	  }
 
-
+	  $(function () {
+		listFilter($("#header"), $("#search-history"));
+	  });
+	}(jQuery));
 
 
 // document.addEventListener("deleteCheck", function () {
